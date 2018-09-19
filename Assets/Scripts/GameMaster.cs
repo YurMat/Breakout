41d67f3627851b267a9a2a3741b7d8aa130c8535
YurMat <yurimatsu28@gmail.com>
2018-09-19 15:48:50 +0900

    complete

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+    public int blockNum;
+    public float nowTime;
+
+    // Use this for initialization
+    void Start() {
+        nowTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        nowTime += Time.deltaTime;
+        if (blockNum <= 0)
+        {
+            GameOver(nowTime.ToString("F0") + "秒");
+        }
+
+    }
+    public void GameOver(string ResultMeseage){
+        DataSender.resultMessage = ResultMeseage;
+        SceneManager.LoadScene("Result");
+    }
+}
